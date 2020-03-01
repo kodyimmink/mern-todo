71bfc2201346e808bb9febe8b99f28df6e505cce
kodyimmink <kodyimmink@gmail.com>
2020-03-01 12:46:33 -0500

    Updates

---
+++ precache-manifest.5d7e3e94194243b8241d1d1491332a06.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e19980b8854d59b74151a5d453c14fc6",
+    "url": "/mern-todo-app/index.html"
+  },
+  {
+    "revision": "55db0f434c62a5a25495",
+    "url": "/mern-todo-app/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "c9a420f4228ba814b535",
+    "url": "/mern-todo-app/static/css/main.a4842e09.chunk.css"
+  },
+  {
+    "revision": "55db0f434c62a5a25495",
+    "url": "/mern-todo-app/static/js/2.f3b82163.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "/mern-todo-app/static/js/2.f3b82163.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c9a420f4228ba814b535",
+    "url": "/mern-todo-app/static/js/main.e08c9912.chunk.js"
+  },
+  {
+    "revision": "a5c06110eb4af573ca04",
+    "url": "/mern-todo-app/static/js/runtime-main.d57554f4.js"
+  },
+  {
+    "revision": "1df80df16a38bb97f9c03fa47089cf4e",
+    "url": "/mern-todo-app/static/media/logo.1df80df1.svg"
+  }
+]);
