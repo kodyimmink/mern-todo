765c8c8c18860e17c4d5280485629312b674e102
kodyimmink <kodyimmink@gmail.com>
2020-03-01 13:42:24 -0500

    reworked directory structure for host and deploy

--- server.js
+++ server.js
@@ -3,12 +3,16 @@ require('dotenv').config();
 
 //Server setup
 const express = require('express');
+const path = require('path');
 const app = express();
 const cors = require('cors');
 const mongoose = require('mongoose');
 const port = process.env.PORT || 5000;
 const connection  = mongoose.connection;
 const dbUri = process.env.ATLAS_URI;
+
+// Serve the static files from the React app
+app.use(express.static(path.join(__dirname, 'client/build')));
 
 //Routes
 const todoRouter = require('./routes/todo.routes.js');
@@ -24,6 +28,11 @@ app.use('/todo', todoRouter);
 mongoose.connect(dbUri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true, useFindAndModify: false });
 connection.once('open', ()=> {
     console.log("MongoDB database connection established successfully")
+});
+
+// Handles any requests that don't match the ones above
+app.get('*', (req, res) =>{
+    res.sendFile(path.join(__dirname+'client/build/index.html'));
 });
 
 //Start server
